d5ceeb62789f96ad0689a31cc4d085b71ec0f3fb
izaak svec <slavi.svec@gmail.com>
2021-06-30 14:56:41 +1000

    Change colum names

--- src/Components/ReactGrid/ReactGrid.jsx
+++ src/Components/ReactGrid/ReactGrid.jsx
@@ -5,12 +5,12 @@ const ReactGrid = () => {
   return (
     <>
       <div class="flex-grid-fours">
-        <div className="col">DIV 1</div>
-        <div className="col">DIV 2</div>
+        <div className="col">volunteers</div>
+        <div className="col">Band Submission</div>
       </div>
       <div class="flex-grid-fours">
-        <div className="col">DIV 3</div>
-        <div className="col">DIV 4</div>
+        <div className="col">Tickets.</div>
+        <div className="col">Flyer</div>
       </div>
     </>
   )
