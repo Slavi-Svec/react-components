06eaec0d1f54ee549e4eb400bfb33aade9bd7fbe
izaak svec <slavi.svec@gmail.com>
2021-07-17 23:53:59 +1000

    Add styles to form

--- src/Components/ContactForm/ContactForm.jsx
+++ src/Components/ContactForm/ContactForm.jsx
@@ -9,6 +9,7 @@ const ContactForm = () => {
   })
 
   const [submitted, setSubmitted] = useState(true)
+  const [valid, setValid] = useState(false)
 
   const handleName = event => {
     setValues({ ...values, name: event.target.value })
@@ -25,46 +26,54 @@ const ContactForm = () => {
   const handleSubmit = event => {
     event.preventDefault()
     if (values.name && values.message && values.email) {
+      setValid(true)
     }
     setSubmitted(true)
   }
 
   return (
-    <div className="form-container">
-      <form className="register-form" onSubmit={handleSubmit}>
-        <input
-          onChange={handleName}
-          value={values.name}
-          className="form-field"
-          placeholder="Name"
-          name="name"
-        />
-        {submitted && !values.name ? <span>Please enter your name</span> : null}
-        <input
-          onChange={handleMessage}
-          value={values.message}
-          className="form-field"
-          placeholder="Message"
-          name="message"
-        />
-        {submitted && !values.message ? (
-          <span>Please enter a message</span>
-        ) : null}
-        <input
-          onChange={handleEmail}
-          value={values.email}
-          className="form-field"
-          placeholder="Email"
-          name="text"
-        />
-        {submitted && !values.email ? (
-          <span>Please enter a valid email</span>
-        ) : null}
-        <button className="form-field" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
+    <section className="form">
+      <div className="form-container">
+        <form className="submit-form" onSubmit={handleSubmit}>
+          {submitted && valid ? (
+            <div className="message-success">Thanks for contacting</div>
+          ) : null}
+          <input
+            onChange={handleName}
+            value={values.name}
+            className="form-field"
+            placeholder="Name"
+            name="name"
+          />
+          {submitted && !values.name ? (
+            <span className="message-warnings">Please enter your name</span>
+          ) : null}
+          <input
+            onChange={handleMessage}
+            value={values.message}
+            className="form-field"
+            placeholder="Message"
+            name="message"
+          />
+          {submitted && !values.message ? (
+            <span className="message-warnings">Please enter a message</span>
+          ) : null}
+          <input
+            onChange={handleEmail}
+            value={values.email}
+            className="form-field"
+            placeholder="Email"
+            name="text"
+          />
+          {submitted && !values.email ? (
+            <span className="message-warnings">Please enter a valid email</span>
+          ) : null}
+          <button className="form-field" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </section>
   )
 }
 
