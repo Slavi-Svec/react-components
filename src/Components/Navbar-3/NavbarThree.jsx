e12793ac82c5157ec3e004040e1fff500206620b
izaak svec <slavi.svec@gmail.com>
2021-07-28 22:04:29 +1000

    Add useState to button

--- src/Components/Navbar-3/NavbarThree.jsx
+++ src/Components/Navbar-3/NavbarThree.jsx
@@ -1,40 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './styles.css'
 
 const Navbar = () => {
   const [click, setClick] = useState(false)
-  const [button, setButton] = useState(true)
-  const [navbar, setNavbar] = useState(false)
-  const [navbarText, setNavbarText] = useState(false)
 
   const handleClick = () => setClick(!click)
   const closeMobileMenu = () => setClick(false)
 
   return (
     <>
-      <nav className={navbar ? 'navbar active' : 'navbar'}>
-        <div className="navbar-container">
-          <div className="menu-icon" onClick={handleClick}>
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className={navbarText ? 'navbarText active' : 'navbarText'}>
-              <li to="/" className="nav-lis" onClick={closeMobileMenu}>
-                About
-              </li>
-            </li>
-            <li className={navbarText ? 'navbarText active' : 'navbarText'}>
-              <li to="/work" className="nav-lis" onClick={closeMobileMenu}>
-                Work
-              </li>
-            </li>
-            <li className={navbarText ? 'navbarText active' : 'navbarText'}>
-              <li to="/contact" className="nav-lis" onClick={closeMobileMenu}>
-                Contact
-              </li>
-            </li>
-          </ul>
+      <nav className="navbar">
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className="navbarText">
+            <li to="/" className="nav-lis" onClick={closeMobileMenu}>
+              About
+            </li>
+          </li>
+          <li className="navbarText">
+            <li to="/work" className="nav-lis" onClick={closeMobileMenu}>
+              Work
+            </li>
+          </li>
+          <li className="navbarText">
+            <li to="/contact" className="nav-lis" onClick={closeMobileMenu}>
+              Contact
+            </li>
+          </li>
+        </ul>
       </nav>
     </>
   )
