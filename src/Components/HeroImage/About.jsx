aeffe195ae2f1ba51fbda163465d977fff19bc3d
izaak svec <slavi.svec@gmail.com>
2021-08-18 22:31:06 +1000

    Update About

---
+++ src/Components/HeroImage/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Text from '../Text/Text'
+import './styles.css'
+
+const About = () => {
+  return <Text text="About" className="about-hero-section" />
+}
+
+export default About
